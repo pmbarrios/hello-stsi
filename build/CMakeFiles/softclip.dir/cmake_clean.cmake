565e1b1daa624f09adc4529507f4625a27dcbeb6
Pablo Moura Barrios <pablomourabarrios@mac.dfci.harvard.edu>
2025-06-12 12:17:14 -0400

    Initial commit: Hello STSI program with CMake

---
+++ build/CMakeFiles/softclip.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/softclip.dir/main.cpp.o"
+  "CMakeFiles/softclip.dir/main.cpp.o.d"
+  "softclip"
+  "softclip.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/softclip.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CMakeFiles/softclip.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for softclip.
+# This may be replaced when dependencies are built.
